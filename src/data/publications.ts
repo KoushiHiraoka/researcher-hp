4a9454d026f9f1635c34f1c67fc5dd729b3e02fb
koushi hiraoka <hiraoka.koushi@arakawa-lab.com>
2026-02-08 21:19:52 +0900

    add: 業績追加とリスト表示の変更

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -1,4 +1,4 @@
-export const myNames = ["平岡滉司", "Koushi Hiraoka", "K. Hiraoka"];
+export const myNames = ["平岡 滉司", "Koushi Hiraoka", "K. Hiraoka"];
 
 export interface Publication {
   title: string;
@@ -7,6 +7,7 @@ export interface Publication {
   year: string;
   month: string;
   presentation?: string;
+  award?: string;
   links?: {
     paper?: string;
     doi?: string;
@@ -36,6 +37,7 @@ export const internationalConferences: Publication[] = [
     year: "2026",
     month: "January",
     links: {
+      paper: "https://doi.org/10.1145/3737611.37769", 
     }
     },
     {
@@ -45,6 +47,7 @@ export const internationalConferences: Publication[] = [
     year: "2026",
     month: "January",
     links: {
+      paper: "https://doi.org/10.1145/3737611.377696",
     }
     },
     {
@@ -54,6 +57,7 @@ export const internationalConferences: Publication[] = [
     year: "2025",
     month: "November",
     links: {
+      paper: "https://epapers2.org/nolta2025/ESR/paper_details.php?paper_id=6089",
     }
     },
     {
@@ -61,12 +65,98 @@ export const internationalConferences: Publication[] = [
     authors: "Koushi Hiraoka, Yugo Nakamura, Yutaka Arakawa",
     venue: "International Conference on Human-Computer Interaction (HCII)",
     year: "2025",
-    month: "June",
+    month: "May",
     links: {
-    //   paper: "https://ieeexplore.ieee.org/document/11367700?denied=",
-    //   doi: "10.1109/ACCESS.2026.3659343",    
+      paper: "https://link.springer.com/chapter/10.1007/978-3-031-92977-9_17",
+    }
+    },
+    {
+    title: "WiLearn: Design and Implementation of a Microlearning System that Utilizes a Captive Portal of Wi-Fi",
+    authors: "Koushi Hiraoka, Shuta Matsuo, Yugo Nakamura, Yutaka Arakawa",
+    venue: "International Conference on Mobile Computing and Ubiquitous Networking (ICMU)",
+    year: "2023",
+    month: "November",
+    links: {
+      paper: "https://ieeexplore.ieee.org/document/10412210",
+      doi: "10.23919/ICMU58504.2023.10412210", 
     }
     },
 ];
 
-export const domesticConferences: Publication[] = [];
+export const domesticConferences: Publication[] = [
+    {
+    title: "IMUとVPUを併用した競歩におけるロス・オブ・コンタクト判定手法の検討",
+    authors: "中山 陽, 平岡 滉司, 中村 優吾, 荒川 豊",
+    venue: "モバイルコンピューティングと新社会システム (MBL) 研究会",
+    year: "2025",
+    month: "November",
+    links: {
+    }
+    },
+    {
+    title: "VPUリングによるテクスチャ認識を活用した指腹部インタラクションの検討 (Demo)",
+    authors: "平岡 滉司, 中村 優吾, 荒川 豊",
+    venue: "第33回 マルチメディア通信と分散処理ワークショップ (DPSWS)",
+    year: "2025",
+    month: "November",
+    award: "最優秀デモンストレーション賞",
+    links: {
+    }
+    },
+    {
+    title: "VPUリングによる表面弾性波を用いた手指接触面テクスチャ認識とロバスト性評価",
+    authors: "平岡 滉司, 中村 優吾, 荒川 豊",
+    venue: "ユビキタスコンピューティングシステム (UBI) 研究会",
+    year: "2025",
+    month: "September",
+    award: "学生奨励賞",
+    links: {
+    }
+    },
+    {
+    title: "ウェアラブルVLMを用いたプライバシーアウェアな行動認識に関する研究",
+    authors: "平岡 滉司, 中村 優吾, 荒川 豊",
+    venue: "UBI Symposium",
+    year: "2025",
+    month: "February",
+    links: {
+    }
+    },
+    {
+    title: "ウェアラブルカメラを用いたプライバシアウェアな行動認識のためのエッジVLMの適用可能性の検討",
+    authors: "平岡 滉司, 中村 優吾, 荒川 豊",
+    venue: "ユビキタスコンピューティングシステム (UBI) 研究会",
+    year: "2024",
+    month: "November",
+    award: "学生奨励賞",
+    links: {
+    }
+    },
+    {
+    title: "小型ウェアラブルカメラとエッジ検出によるプライバシーアウェアな行動認識システムの提案 (Demo)",
+    authors: "平岡 滉司, 中村 優吾, 荒川 豊",
+    venue: "第32回 マルチメディア通信と分散処理ワークショップ (DPSWS)",
+    year: "2024",
+    month: "November",
+    links: {
+    }
+    },
+    {
+    title: "小型ウェアラブルカメラを用いたプライバシーアウェアな行動認識の実現に向けた基礎調査",
+    authors: "平岡 滉司, 中村 優吾, 荒川 豊",
+    venue: "ユビキタスコンピューティングシステム (UBI) 研究会",
+    year: "2024",
+    month: "September",
+    links: {
+    }
+    },
+    {
+    title: "カーシェアユーザの行動変容を促す音声介入手法の検討",
+    authors: "平岡 滉司, 本松大夢, 谷澤健太, 酒井鴻，荒川豊",
+    venue: "第109回モバイルコンピューティングと新社会システム(MBL)・第95回高度交通システムとスマートコミュニティ(ITS)合同研究発表会",
+    year: "2023",
+    month: "November",
+    links: {
+    }
+    },
+];
