5d3e9189f99a896f8013375dd04298a0c859b536
koushi hiraoka <hiraoka.koushi@arakawa-lab.com>
2026-02-27 11:41:52 +0800

    add: profiles

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -23,6 +23,16 @@ export const researchInterests = [
 ];
 
 export const grants: { title: string; organization: string; period: string; link?: string }[] = [
+    {   title: "PARKS アントレプレナー教育学⽣海外派遣プログラム", 
+        organization: "PARKS (Platform for All Regions of Kyushu & Okinawa for Startup-ecosystem)", 
+        period : "2026年2月-2026年3月", 
+        link: "https://www.parks-startup.jp/"
+    },
+    {   title: "異能vation ジェネレーションアワード ノミネート \n 採択課題：あなたをコントローラーに「SAW-Ring」", 
+        organization: "2025年度 異能vation", 
+        period : "2026年2月", 
+        link: "https://inno-vation.jp/nominate/2025/"
+    },
     {   title: "2025年度 福岡未踏 【Pro】採択 \n「SAW-Ring：表面弾性波センシングを活用したテクスチャ駆動リング型デバイス」", 
         organization: "福岡未踏的人材発掘・育成コンソーシアム (福岡未踏)", 
         period : "2025年7月 - 2026年2月", 
@@ -39,6 +49,11 @@ export const grants: { title: string; organization: string; period: string; link
     {   title: "2024年度 日本学生支援機構奨学金 大学院「特に優れた業績による返還免除」", 
         organization: "日本学生支援機構", 
         period : "2023年4月 - 2025年3月" 
+    },
+    {   title: "PolyU International Research Summer School 2024", 
+        organization: "香港理工大学（PolyU）", 
+        period : "2024年7月",
+        link: "https://www.polyu.edu.hk/gs/news-and-events/news/2024/0701-13_irss-2024/"
     },
 ];
 
